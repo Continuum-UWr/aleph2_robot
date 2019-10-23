d712d62411c74ea94c982684b8f4e27e0b46d19a
Arusekk <arek_koz@o2.pl>
2019-10-24 00:46:00 +0200

    Próba odpalenia CI

---
+++ gitlab-ci-buildscript.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -ex
+
+set +x # po co wyświetlać token
+token=`grep 'ros_ws\.git' .git/config | grep -o 'https\?://gitlab-ci-token:.*/continuum/' | head -1`
+sed -i "s git@.*:continuum/ $token g" $1
+set -x
+
+unset token
+rm -rf src
+mkdir -p ws/src && ln -s ../.. ws/src/${PWD##*/}
+wstool init ws/src $1
+cd ws
+wstool update -t src
+apt update
+rosdep update
+rosdep install --from-paths src --ignore-src -y
+catkin build --summarize --no-status --force-color --cmake-args -DCMAKE_C_FLAGS="-Wall -W -Wno-unused-parameter"
+source devel/setup.bash
