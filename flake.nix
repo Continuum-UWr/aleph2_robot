66bcd20c729cc15f67b0344260a9a8240e89612e
Błażej Sowa <bsowa123@gmail.com>
2024-04-24 01:57:15 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    nix-ros-overlay = {
-      url =
-        "git+https://gitlab.continuum.ii.uni.wroc.pl/continuum/software/nix-ros-overlay?ref=continuum";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
+    nixpkgs.follows = "aleph2_common/nixpkgs";
+    flake-utils.follows = "aleph2_common/flake-utils";
+    nix-ros-overlay.follows = "aleph2_common/nix-ros-overlay";
     aleph2_common.url =
       "git+https://gitlab.continuum.ii.uni.wroc.pl/continuum/software/aleph2_common";
   };
-  outputs = { self, nixpkgs, flake-utils, nix-ros-overlay }:
+  outputs = { self, nixpkgs, flake-utils, nix-ros-overlay, aleph2_common }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs {
@@ -24,14 +19,11 @@
         }).pkgs.rosPackages.rolling;
 
         aleph2_bringup = ros.callPackage (import ./aleph2_bringup) { };
-        # nanotec_driver = ros.callPackage (import ./nanotec_driver) { };
 
       in {
         packages = {
-          # inherit aleph2_bringup nanotec_driver;
           inherit aleph2_bringup;
           default = aleph2_bringup;
-
         };
         devShells.default = pkgs.mkShell {
           inputsFrom = [ aleph2_bringup ];
