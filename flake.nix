c72dccf6c7d2fefabfc800481fa97c6cfcadf541
Błażej Sowa <bsowa123@gmail.com>
2024-04-24 23:51:08 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     flake-utils.follows = "aleph2-common/flake-utils";
     nix-ros-overlay.follows = "aleph2-common/nix-ros-overlay";
     aleph2-common.url =
-      "git+https://gitlab.continuum.ii.uni.wroc.pl/continuum/software/aleph2_common?ref=nix-update";
+      "git+https://gitlab.continuum.ii.uni.wroc.pl/continuum/software/aleph2_common";
   };
   outputs = { self, nixpkgs, flake-utils, nix-ros-overlay, aleph2-common }:
     flake-utils.lib.eachDefaultSystem (system:
