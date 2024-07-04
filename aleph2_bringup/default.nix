f755d2785d741347b5957a43b06e18f29906525f
Błażej Sowa <bsowa123@gmail.com>
2024-07-05 00:22:32 +0200

    Add missing exec dependencies

--- aleph2_bringup/default.nix
+++ aleph2_bringup/default.nix
@@ -1,6 +1,7 @@
 { buildRosPackage, ament-cmake-ros, lely-core-libraries, aleph2-description
-, nanotec-driver, xacro, ament-lint-auto, ament-cmake-lint-cmake
-, ament-cmake-xmllint }:
+, canopen-core, canopen-master-driver, diff-drive-controller
+, joint-state-broadcaster, nanotec-driver, xacro, ament-lint-auto
+, ament-cmake-lint-cmake, ament-cmake-xmllint }:
 buildRosPackage {
   pname = "aleph2-bringup";
   version = "0.0.0";
@@ -11,7 +12,15 @@ buildRosPackage {
 
   buildType = "ament_cmake";
   buildInputs = [ ament-cmake-ros lely-core-libraries ];
-  propagatedBuildInputs = [ xacro aleph2-description nanotec-driver ];
+  propagatedBuildInputs = [
+    aleph2-description
+    canopen-core
+    canopen-master-driver
+    diff-drive-controller
+    joint-state-broadcaster
+    nanotec-driver
+    xacro
+  ];
   checkInputs = [ ament-lint-auto ament-cmake-lint-cmake ament-cmake-xmllint ];
   nativeBuildInputs = [ ament-cmake-ros ];
 }
